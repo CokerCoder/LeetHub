aba5367432a2382be226974b9d969566499392ec
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-05 18:43:10 +1000

    Time: 0 ms (100.00%), Space: 39.1 MB (6.20%) - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,28 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length - 1;
-        int new_num = digits[n] + 1;
-        if (new_num < 10) {
-            digits[n] = new_num;
-        } else {
-            digits[n] = 0;
-            int bring = 1;
-            int i = n - 1;
-            while (bring > 0) {
-                if (i < 0) {
-                    int[] new_digits = new int[digits.length+1];
-                    new_digits[0] = 1;
-                    return new_digits;
-                }
-                int new_prev = digits[i] + bring;
-                if (new_prev < 10) {
-                    digits[i] = new_prev;
-                    bring = 0;
-                } else {
-                    digits[i--] = 0;
-                }
+        int n = digits.length;
+        for (int i=n-1; i>=0; i--) {
+            int new_num = digits[i] + 1;
+            if (new_num < 10) {
+                digits[i] = new_num;
+                return digits;
+            } else {
+                digits[i] = 0;
             }
         }
-        return digits;
+        int new_digits[] = new int[n+1];
+        new_digits[0] = 1;
+        return new_digits;
     }
 }
