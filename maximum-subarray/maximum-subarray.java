08a6f5451c62bc151d68f0a214f3d240ff19bb9a
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-07 15:27:44 +1000

    Time: 1 ms (47.93%), Space: 39.2 MB (22.33%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -3,20 +3,15 @@ class Solution {
         
         // dynamic programming
         int len = nums.length;
-        // local_maxi[x] is the local maximum of the subarrays starting at position x
-        int[] local_maxi = new int[len];
-        
-        int global_maxi = nums[0];
-        
-        local_maxi[0] = nums[0];
-        
+        int global_maxi = nums[0], local_maxi = nums[0];
+
         // calculate the sums of subarrays ending at position i
         for (int i=1; i<len; i++) {
             // local_maxi[i] = max(nums[i], local_maxi[i-1])
-            int next = Math.max(nums[i], nums[i]+local_maxi[i-1]);
+            int next = Math.max(nums[i], nums[i]+local_maxi);
             if (next > global_maxi)
                 global_maxi = next;
-            local_maxi[i] = next;
+            local_maxi = next;
         }
         
         return global_maxi;
