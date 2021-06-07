c4f152cbf2acd435d9ebaaf51cc7b46ef14c5f1c
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-07 21:04:23 +1000

    Time: 0 ms (100.00%), Space: 35.7 MB (58.51%) - LeetHub

--- climbing-stairs/climbing-stairs.java
+++ climbing-stairs/climbing-stairs.java
@@ -1,19 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] steps = new int[n];
-        if (n==1)
-            return 1;
-        if (n==2)
-            return 2;
-        if (n==3)
-            return 3;
         
+        if (n==1 || n==2)
+            return n;
+        
+        int[] steps = new int[n];
         // steps[i] stores how many steps there are to reach ith floor
         steps[0] = 1;
         steps[1] = 2;
-        steps[2] = 3;
+        
         // steps[i] = steps[i-1] + steps[i-2]
-        for (int i=3; i<n; i++) {
+        for (int i=2; i<n; i++) {
             steps[i] = steps[i-1] + steps[i-2];
         }
         
