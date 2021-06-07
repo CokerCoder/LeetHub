4b05dcf8246dd1092394cb8468907407d63ee3ea
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-07 16:10:18 +1000

    Time: 3 ms (18.75%), Space: 105.7 MB (6.15%) - LeetHub

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxProfit(int[] prices) {
         
         int n = prices.length;
-        int[] cheapest_stocks = new int[n];
         int cheapest_stock = prices[0];
         int max_profit = 0;
         
@@ -10,7 +9,6 @@ class Solution {
             if (prices[i] < cheapest_stock) {
                 cheapest_stock = prices[i];
             }
-            cheapest_stocks[i] = cheapest_stock;
             
             int profit = prices[i] - cheapest_stock;
             if (profit > max_profit)
