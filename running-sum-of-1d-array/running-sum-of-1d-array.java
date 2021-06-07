aae43915b115b741d6f163763e736ac081a69b19
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-07 23:43:42 +1000

    Time: 0 ms (100.00%), Space: 38.8 MB (81.84%) - LeetHub

--- running-sum-of-1d-array/running-sum-of-1d-array.java
+++ running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int n = nums.length;
-        int[] sums = new int[n];
-        sums[0] = nums[0];
-        for (int i=1; i<n; i++) {
-            sums[i] = nums[i] + sums[i-1];
+        for (int i=1; i<nums.length; i++) {
+            nums[i] = nums[i] + nums[i-1];
         }
-        return sums;
+        return nums;
     }
 }
