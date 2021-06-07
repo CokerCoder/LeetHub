d9a5a76f8031fcd2282d1766de5aee02d0a5c379
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-07 15:21:50 +1000

    Time: 2 ms (14.11%), Space: 41.6 MB (6.18%) - LeetHub

--- maximum-subarray/maximum-subarray.java
+++ maximum-subarray/maximum-subarray.java
@@ -1,16 +1,24 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // brute-force
-        int maxi = Integer.MIN_VALUE;
-        for (int i=0; i<nums.length; i++) {
-            int curr = 0;
-            for (int j=i; j<nums.length; j++) {
-                curr += nums[j];
-                if (curr > maxi)
-                    maxi = curr;
-            }
+        
+        // dynamic programming
+        int len = nums.length;
+        // local_maxi[x] is the local maximum of the subarrays starting at position x
+        int[] local_maxi = new int[len];
+        
+        int global_maxi = nums[0];
+        
+        local_maxi[0] = nums[0];
+        
+        // calculate the sums of subarrays ending at position i
+        for (int i=1; i<len; i++) {
+            // local_maxi[i] = max(nums[i], local_maxi[i-1])
+            int next = Math.max(nums[i], nums[i]+local_maxi[i-1]);
+            if (next > global_maxi)
+                global_maxi = next;
+            local_maxi[i] = next;
         }
         
-        return maxi;
+        return global_maxi;
     }
 }
