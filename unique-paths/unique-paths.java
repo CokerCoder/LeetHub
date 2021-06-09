2e570243409913b6ef6fd5cdd74ce26cb9bde203
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-09 15:03:42 +1000

    Time: 0 ms (100.00%), Space: 35.9 MB (32.60%) - LeetHub

--- unique-paths/unique-paths.java
+++ unique-paths/unique-paths.java
@@ -1,14 +1,14 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] A = new int[m][n];
-        // initialize the first row and first column
-        for (int i=0; i<m; i++) 
-            A[i][0] = 1;
-        for (int j=0; j<n; j++) 
-            A[0][j] = 1;
-        for (int row=1; row<m; row++) 
-            for (int col=1; col<n; col++)
-                A[row][col] = A[row-1][col] + A[row][col-1];
+        for (int row=0; row<m; row++) {
+            for (int col=0; col<n; col++) {
+                if (row==0 || col==0)
+                    A[row][col] = 1;
+                else
+                    A[row][col] = A[row-1][col] + A[row][col-1];
+            }
+        }
         return A[m-1][n-1];
     }
 }
