b21af17169f52dfb0667372adce3f255d6e0bfed
Yunfei Jing <yunfei.jing@outlook.com>
2021-06-22 11:00:24 +1000

    Time: 1 ms (81.27%), Space: 36.3 MB (52.23%) - LeetHub

--- combination-sum-iv/combination-sum-iv.java
+++ combination-sum-iv/combination-sum-iv.java
@@ -2,15 +2,10 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         
         int[] dp = new int[target+1];
-        
-        int min = Arrays.stream(nums)
-                        .min()
-                        .getAsInt();
-        if (min > target) return 0;
+
         dp[0] = 1; // add itself
-        dp[min] = 1;
         
-        for (int i=min+1; i<=target; i++) {
+        for (int i=0; i<=target; i++) {
             for (int j=0; j<nums.length; j++) {
                 if (i - nums[j] >= 0) dp[i] += dp[i - nums[j]];
             }
